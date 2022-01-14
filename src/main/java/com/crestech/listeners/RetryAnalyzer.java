72b9a03cae7a9d7eaeabac442429ffdf0e9bc735
Admin <Admin@CTNL0060.crestechad.com>
2022-01-14 19:18:30 +0530

    Cron Job Changes And Retry Analyzer. Signed-off-by: Admin <Admin@CTNL0060.crestechad.com>

--- src/main/java/com/crestech/listeners/RetryAnalyzer.java
+++ src/main/java/com/crestech/listeners/RetryAnalyzer.java
@@ -9,13 +9,15 @@ public class RetryAnalyzer implements IRetryAnalyzer {
     private static int maxTry = 1; //Run the failed test 2 times
 
     @Override
-    public boolean retry(ITestResult iTestResult) {
-        if (!iTestResult.isSuccess() && count < maxTry) {                      //Check if test not succeed
-        count++;                                     //Increase the maxTry count by 1
-                iTestResult.setStatus(ITestResult.FAILURE);  //Mark test as failed and take base64Screenshot
-                return true;                                 //Tells TestNG to re-run the test
-            }
-        return false;
-    }
+	public boolean retry(ITestResult iTestResult) {
+		if (!iTestResult.isSuccess() && count < maxTry) { // Check if test not succeed
+			iTestResult.getTestContext().getSkippedTests().removeResult(iTestResult);
+			count++; // Increase the maxTry count by 1
+			// iTestResult.setStatus(ITestResult.FAILURE); //Mark test as failed and take
+			// base64Screenshot
+			return true; // Tells TestNG to re-run the test
+		}
+		return false;
+	}
 
 }
